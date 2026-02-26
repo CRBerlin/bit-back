71fb674b265dfd2fabfdb8038f4bd6fdf4856529
CRBerlin <crdq17@hotmail.com>
2026-02-26 11:48:35 -0500

    CRUD Usuarios probado OK.

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 Este es el repositorio para crear el proyecto de bit con nodejs
+
+Para hacer pruebas con Postman:
+
+Crear usuario ->
+
+localhost:3000/usuarios
+
+{
+    "name": "Carlos Roberto",
+    "email":"crdq17@hotmail.com",
+    "password":"1234",
+    "role":"usuario",
+    "plan":"basico",
+    "planStatus":"activo",
+    "trainerId":"1",
+    "currentSessionIndex":"",
+    "streakCount":"",
+    "lasCompletedDate":""
+}
+
+Leer Usuarios ->
+
+localhost:3000/usuarios
+
+Leer un Usuario ->
+
+localhost:3000/usuarios/2
+
+Acutaliza Usuario ->
+
+localhost:3000/usuarios/1
+
+{
+    "name": "CarlosR.",
+    "email":"crdq17@hotmail.com",
+    "password":"1234",
+    "role":"usuario",
+    "plan":"basico",
+    "planStatus":"activo",
+    "trainerId":"1",
+    "currentSessionIndex":"",
+    "streakCount":"",
+    "lasCompletedDate":""
+}
+
+Borrar Usuarios ->
+
+localhost:3000/usuarios/1
--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,93 @@
 const express = require('express')
-const app = express()
+//Aplicación móvil
+const satori = express()
 const port = 3000
+satori.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+//#region CRUD Usuarios
+const users = [];
+
+//Crear Usuario
+satori.post('/usuarios', (req, res) => {
+  const { name, email, password, role, plan, planStatus, trainerId, currentSessionIndex, streakCount, lasCompletedDate } = req.body;
+  if (!name || !email || !password || !role || trainerId === undefined) {
+    return res.status(400).json({ message: 'Falta campo obligatorio' });
+  }
+  const newUser = {
+    id: users.length + 1,
+    name,
+    email,
+    password,
+    role,
+    plan,
+    planStatus,
+    trainerId,
+    currentSessionIndex,
+    streakCount,
+    lasCompletedDate
+  };
+  users.push(newUser);
+  res.status(201).json(newUser);
 })
 
-app.listen(port, () => {
+//Leer Usuarios
+satori.get('/usuarios', (req, res) => {
+  res.status(200).json(users);
+})
+
+//Leer un usuario por ID
+satori.get('/usuarios/:id', (req, res) => {
+  const id = Number.parseInt(req.params.id);
+  const userId = users.find((user) => user.id === id);
+  if (userId) {
+    res.status(200).json(userId);
+  } else {
+    res.status(404).json({ message: 'Usuario no encontrado' });
+  }
+});
+
+// actualizar producto
+satori.put('/usuarios/:id', (req, res) => {
+  const id = Number.parseInt(req.params.id);
+  const userId = users.findIndex((user) => user.id === id);
+  if (userId === -1) {
+    return res.status(404).json({ message: 'Usuario no encontrado' });
+  }
+
+  const { name, email, password, role, plan, planStatus, trainerId, currentSessionIndex, streakCount, lasCompletedDate } = req.body;
+  if (!name || !email || !password || !role || trainerId === undefined) {
+    return res.status(400).json({ message: 'Falta campo obligatorio' });
+  }
+  const updatedUser = {
+    id,
+    name,
+    email,
+    password,
+    role,
+    plan,
+    planStatus,
+    trainerId,
+    currentSessionIndex,
+    streakCount,
+    lasCompletedDate
+  };
+  users[userId] = updatedUser;
+  res.status(200).json(updatedUser);
+});
+
+// eliminar producto
+satori.delete('/usuarios/:id', (req, res) => {
+  const id = Number.parseInt(req.params.id);
+  const userId = users.findIndex((user) => user.id === id);
+  if (userId === -1) {
+    return res.status(404).json({ message: 'Usuario no encontrado' });
+  }
+  users.splice(userId, 1);
+  res.status(204).send();
+});
+//#endregion
+
+// Verificación que el servidor esté en línea.
+satori.listen(port, () => {
   console.log(`Servidor OK en el puerto: ${port}`)
 })
